211474e6ca6f2a63469c1aa6e5c3116933e080fd
arjundoshi221 <arjundoshi221@gmail.com>
2021-08-13 22:42:09 +0530

    commit

--- Python/P4E/Ch13.py
+++ Python/P4E/Ch13.py
@@ -1,49 +1,24 @@
 import urllib.request
-import urllib.parse
-import urllib.error
 import xml.etree.ElementTree as ET
 import ssl
 
-print("hello world")
-
-api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
-
-if api_key is False:
-    api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
-else:
-    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
-
-# Ignore SSL certificate errors
+# ignore SSL Certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    address = input('Enter location: ')
-    if len(address) < 1:
-        break
+sum = 0
+url = input("Enter URL: ")
+fhand = urllib.request.urlopen(url).read()
+print('Retrieved', len(fhand), 'characters')
+# print(fhand.decode())
+tree = ET.fromstring(fhand)
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False:
-        parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
+comment = tree.findall('.//count')
+print("User count: ", len(comment))
 
-    data = uh.read()
-    print('Retrieved', len(data), 'characters')
-    print(data.decode())
-    tree = ET.fromstring(data)
+for numbers in comment:
+    # print(numbers.text)
+    sum += int(numbers.text)
 
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
-
-    print('lat', lat, 'lng', lng)
-    print(location)
+print(sum)
