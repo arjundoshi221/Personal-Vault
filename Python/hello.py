54a4eaad1d58b62b0af435ef8e12344194f47040
arjundoshi221 <arjundoshi221@gmail.com>
2021-08-28 02:37:35 +0530

    commit

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,9 @@
-import klib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df_train = pd.read_csv(r'C:/Users/arjd2/OneDrive/Desktop/Train.csv')
+df_test = pd.read_csv(r'C:/Users/arjd2/OneDrive/Desktop/Test.csv')
+
+print(df_train)
