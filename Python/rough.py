acbb30a6fd111d3ca9946ccbf8e2bd8ea7378ed8
arjundoshi221 <arjundoshi221@gmail.com>
2021-11-24 17:01:36 +0530

    commit

--- Python/rough.py
+++ Python/rough.py
@@ -1,76 +1,39 @@
-from sympy import isprime
-import math
+
+INT_MAX = 4294967296
+INT_MIN = -4294967296
 
 
-def key_maker(p, q):
-    n = p * q
-    phi = (p - 1) * (q - 1)
+class Node:
 
-    e = int(input())
-    while e_gcd(e, phi) == False:
-        print("Not relative prime")
-        e = int(input())
-
-    d = 1
-    while True:
-        if (e * d % phi) == 1:
-            break
-        d += 1
-
-    # print(n, phi, e, d)
-
-    return [e, n], [d, n]
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
 
-def e_gcd(e, phi):
-    if math.gcd(e, phi) == 1:
+def isBST(node):
+    return (isBSTUtil(node, INT_MIN, INT_MAX))
+
+
+def isBSTUtil(node, mini, maxi):
+
+    if node is None:
         return True
-    else:
+
+    if node.data < mini or node.data > maxi:
         return False
 
-
-def encryption(pt, e, n):
-    li = list(pt)
-    num = []
-    for i in range(len(li)):
-        a = ord(li[i]) - 65
-        num.append(a)
-    encrypted = []
-    for m in num:
-        encrypted.append(m ** e % n)
-
-    return encrypted
+    return (isBSTUtil(node.left, mini, node.data - 1) and
+            isBSTUtil(node.right, node.data+1, maxi))
 
 
-def decryption(ct, d, n):
-    decrypted = []
-    for c in ct:
-        decrypted.append(c ** d % n)
-    pt = ""
-    for i in decrypted:
-        a = chr(i + 65)
-        pt += a
+root = Node(4)
+root.left = Node(2)
+root.right = Node(5)
+root.left.left = Node(1)
+root.left.right = Node(3)
 
-    return pt
-
-
-p, q = map(int, input().split())
-while isprime(p) == False or isprime(q) == False:
-    print("Not prime")
-    p, q = map(int, input().split())
-
-publick, privatek = key_maker(p, q)
-# print(publick, privatek)
-
-print("1: encryption 2: decryption")
-ch = int(input())
-
-if ch == 1:
-    pt = input("In Caps please\n")
-    encrypt = encryption(pt, *publick)
-    print(*encrypt)
-
-elif ch == 2:
-    ct = list(map(int, input().split()))
-    decrypt = decryption(ct, *privatek)
-    print(decrypt)
+if (isBST(root)):
+    print("Is BST")
+else:
+    print("Not a BST")
