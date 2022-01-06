5c77d820794dc0c8256fa64a5030f541e197fc0d
arjundoshi221 <arjundoshi221@gmail.com>
2022-01-06 18:45:25 +0530

    commit

--- Java/Andorid App Dev/src/Assignment1/Armstrong.java
+++ Java/Andorid App Dev/src/Assignment1/Armstrong.java
@@ -1,4 +1,4 @@
-package college;
+package src.Assignment1;
 
 import java.util.Scanner;
 
@@ -10,20 +10,19 @@ public class Armstrong {
         int n = Obj.nextInt();
         int temp = n;
         int x = 0, sum = 0;
-        while(temp>0){ 
+        while (temp > 0) {
             ++x;
-            temp = temp/10;
+            temp = temp / 10;
         }
         temp = n;
-        while(temp>0){
-            sum += (Math.pow(temp%10,x));
-            temp = temp/10;
+        while (temp > 0) {
+            sum += (Math.pow(temp % 10, x));
+            temp = temp / 10;
         }
-        if (n == sum){
-            System.out.println(n+" is an Armstrong number");
-        }
-        else{
-            System.out.println(n+" is NOT an Armstrong number");
+        if (n == sum) {
+            System.out.println(n + " is an Armstrong number");
+        } else {
+            System.out.println(n + " is NOT an Armstrong number");
         }
         Obj.close();
     }
