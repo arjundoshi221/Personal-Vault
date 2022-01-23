b6bbd4056b7056d83e770c67ff69231199637018
arjundoshi221 <arjundoshi221@gmail.com>
2022-01-23 15:10:31 +0530

    commit

--- Java/src/AAD/Assignment1/ArmstrongEvenOdd.java
+++ Java/src/AAD/Assignment1/ArmstrongEvenOdd.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
-public class Armstrong {
+public class ArmstrongEvenOdd {
     public static void main(String[] args) {
         Scanner Obj = new Scanner(System.in);
         System.out.println("Enter a number: ");
 
         int n = Obj.nextInt();
         int temp = n;
+
+        if (n % 2 == 1) {
+            System.out.println(n + " is an Odd Number");
+        } else {
+            System.out.println(n + " is an Even Number");
+        }
+
         int x = 0, sum = 0;
         while (temp > 0) {
             ++x;
